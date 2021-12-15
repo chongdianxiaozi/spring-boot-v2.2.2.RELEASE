64b1ed2ccbc147a168a0f48f2c61c5c9bff3c3d0
zhujian <zhujian@ewell.cc>
2021-12-15 10:16:21 +0800

    [测试-spring-boot-tests工程下新建module]

---
+++ spring-boot-tests/spring-boot-chongdianxiaozi-tests/spring-boot-chongdianxiaozi-mvc-tests/src/main/java/com/cdxz/MyTestMVCApplication.java
@@ -0,0 +1,13 @@
+package com.cdxz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTestMVCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTestMVCApplication.class, args);
+	}
+
+}
---
+++ spring-boot-tests/spring-boot-chongdianxiaozi-tests/spring-boot-chongdianxiaozi-mvc-tests/src/main/java/com/cdxz/controller/MyTestController.java
@@ -0,0 +1,17 @@
+package com.cdxz.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+public class MyTestController {
+
+	@ResponseBody
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello world";
+	}
+}
